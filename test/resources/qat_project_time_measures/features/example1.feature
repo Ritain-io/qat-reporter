ef77190f9d2e4e35f32e1795056f01315c348fe0
Sérgio <sergio.matos@readinessit.com>
2019-09-03 15:02:34 +0100

    # refactored scenarios

--- test/resources/qat_project_time_measures/features/example1.feature
+++ test/resources/qat_project_time_measures/features/example1.feature
@@ -16,14 +16,16 @@ Feature: Time measure
       | good   | good   |
 
   @no_start_time
-  Scenario: Take a time measurement 1
-    When the user starts a time measurement without configuration
+  Scenario: Failed time measurement without configuration
+    Given the user starts a time measurement without configuration
     And executes a code snippet that lasts "2" seconds
     And the user stops the time measurement
-    Then a time interval of "2" seconds was measured
-    And the execution time is formatted as "00m 02s"
+    When the time report is generated
+    Then an "QAT::Reporter::Times::NoLabelInConfig" exception is raised
+
 
   @no_start_time
-  Scenario: Take a time measurement 2
-    And executes a code snippet that lasts "2" seconds
-    And the user stops the time measurement
+  Scenario: Failed time measurement without start time
+    When executes a code snippet that lasts "2" seconds
+    And the user stops a blocking time measurement
+    Then an "QAT::Reporter::Times::NoStartTimeError" exception is raised
--- test/resources/qat_project_time_measures/features/step_definitions/steps.rb
+++ test/resources/qat_project_time_measures/features/step_definitions/steps.rb
@@ -42,3 +42,45 @@ Then(/^"(good|bad)" result is achieved/) do |result|
     assert false
   end
 end
+
+Then /^an? "([^"]*)" exception is raised$/ do |klass|
+  refute_nil @error, "Expected a #{klass} exception to have occured, but none was found"
+  log.debug @error
+  assert_equal klass, @error.class.to_s
+end
+
+
+When(/^the time report is (?:generated|sent to the remote server)$/) do
+  begin
+    QAT::Reporter::Times.generate_time_report(QAT[:current_test_id])
+  rescue => @error
+  end
+end
+
+
+When(/^a code snippet that lasts "([^"]*)" seconds is measured$/) do |duration|
+  if @label.eql? nil
+    @label = 'test_measure'
+  else
+    @label
+  end
+
+  QAT::Reporter::Times.measure(@label) do
+    sleep(duration)
+  end
+end
+
+
+
+And(/^the user stops (?:a|the)( blocking)? time measurement$/) do |blocking|
+  begin
+    if blocking
+      QAT::Reporter::Times.stop!(@label)
+    else
+
+      QAT::Reporter::Times.stop(@label)
+    end
+  rescue => @error
+    log.warn "Caught exception: [#{@error.class}] #{@error.message}\n#{@error.backtrace.join("\n")}"
+  end
+end
