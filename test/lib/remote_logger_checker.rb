69b998dc0ba384b74c678ba91993693a642cbda3
Pedro Julio <pedro.julio@readinessit.com>
2019-10-17 17:58:15 +0100

    add test_times , add get_test_times

--- test/lib/remote_logger_checker.rb
+++ test/lib/remote_logger_checker.rb
@@ -18,7 +18,7 @@ class RemoteLoggerChecker
   end
 
   def get_test_results
-    url = "#{remote_logging_url}/#{remote_logging[:index]}-#{Time.now.utc.strftime('%Y.%m.%d')}/_search"
+    url = "#{remote_logging_url}/#{remote_logging[:test_results]['index']}-#{Time.now.utc.strftime('%Y.%m.%d')}/_search"
 
     body = <<-JSON
 {
@@ -28,7 +28,7 @@ class RemoteLoggerChecker
         {
           "query_string": {
             "default_field": "facility",
-            "query": "#{remote_logging[:facility]}"
+            "query": "#{remote_logging[:test_results]['facility']}"
           }
         },
         {
@@ -45,28 +45,66 @@ class RemoteLoggerChecker
 }
     JSON
 
-    Log4r::Logger.log_internal { "ES Request:\nURL:#{url}\n#{body}" }
+    log_timestamp url, body
+  end
 
-    obj  = []
+  def get_test_times
+    url = "#{remote_logging_url}/#{remote_logging[:test_times]['index']}-#{Time.now.utc.strftime('%Y.%m.%d')}/_search"
+
+    body = <<-JSON
+{
+  "query": {
+    "bool": {
+      "must": [
+        {
+          "query_string": {
+            "default_field": "facility",
+            "query": "#{remote_logging[:test_times]['facility']}"
+          }
+        },
+        {
+          "range": {
+            "@timestamp": {
+              "from": "#{@test_start_ts}",
+              "to": "#{Time.now.iso8601(3)}"
+            }
+          }
+        }
+      ]
+    }
+  }
+}
+    JSON
+
+    log_timestamp url, body
+
+  end
+
+  private
+
+  def log_timestamp (url, body)
+    Log4r::Logger.log_internal {"ES Request:\nURL:#{url}\n#{body}"}
+
+    obj = []
     opts = {
-      body:    body,
-      headers: {
-        'Content-Type' => 'application/json',
-        'Accept'       => 'application/json'
-      }
+        body: body,
+        headers: {
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json'
+        }
     }
 
     if ENV['ES_USER'] and ENV['ES_PASSWD']
-      opts[:basic_auth] = { username: ENV['ES_USER'], password: ENV['ES_PASSWD'] }
+      opts[:basic_auth] = {username: ENV['ES_USER'], password: ENV['ES_PASSWD']}
     end
 
-    Retriable.retriable on:            [NoESLogFound],
-                        tries:         30,
+    Retriable.retriable on: [NoESLogFound],
+                        tries: 30,
                         base_interval: 0.5,
-                        multiplier:    1.0,
-                        rand_factor:   0.0 do
+                        multiplier: 1.0,
+                        rand_factor: 0.0 do
       result = HTTParty.get(url, opts).body
-      Log4r::Logger.log_internal { "Result: #{result}" }
+      Log4r::Logger.log_internal {"Result: #{result}"}
 
       result = JSON.parse result
 
